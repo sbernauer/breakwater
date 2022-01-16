891dda3e6d129239163bef2186d89a6c3d4e6f0e
Sebastian Bernauer <bernauerse@web.de>
2022-01-16 14:59:07 +0100

    Only count connections per ip, not also all together

--- src/statistics.rs
+++ src/statistics.rs
@@ -5,29 +5,23 @@ use std::sync::atomic::Ordering::Relaxed;
 use std::sync::Mutex;
 
 pub struct Statistics {
-    connections: AtomicU32,
     connections_for_ip: Mutex<HashMap<IpAddr, AtomicU32>>,
 }
 
 impl Statistics {
     pub fn new() -> Self {
         Statistics {
-            connections: AtomicU32::new(0),
             connections_for_ip: Mutex::new(HashMap::new()),
         }
     }
 
     pub fn inc_connections(&self, ip: IpAddr) {
-        self.connections.fetch_add(1, Relaxed);
-
         let mut connections_for_ip = self.connections_for_ip.lock().unwrap();
         connections_for_ip.entry(ip).or_insert(AtomicU32::new(0));
         connections_for_ip[&ip].fetch_add(1, Relaxed);
     }
 
     pub fn dec_connections(&self, ip: IpAddr) {
-        self.connections.fetch_sub(1, Relaxed);
-
         let mut connections_for_ip = self.connections_for_ip.lock().unwrap();
         let remaining_connections_for_ip = connections_for_ip[&ip].fetch_sub(1, Relaxed);
         if remaining_connections_for_ip <= 1 { // We check for 1 instead of 1 as we get the value before decrementing
@@ -36,7 +30,10 @@ impl Statistics {
     }
 
     pub fn get_connections(&self) -> u32 {
-        self.connections.load(Relaxed)
+        self.connections_for_ip.lock().unwrap()
+            .values()
+            .map(|i| i.load(Relaxed))
+            .sum()
     }
 
     pub fn get_ip_count(&self) -> u32 {
