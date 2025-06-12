35b920533df67e9d440c081738b253341a74de7d
Sebastian Bernauer <sebastian.bernauer@stackable.tech>
2025-06-12 12:18:20 +0200

    clippy

--- breakwater-parser-c-bindings/src/lib.rs
+++ breakwater-parser-c-bindings/src/lib.rs
@@ -19,6 +19,10 @@ static ORIGINAL_PARSER: OnceLock<Mutex<OriginalParser<SharedMemoryFrameBuffer>>>
 /// 1 `width` (`int`): The width of the canvas in pixels
 /// 2 `height`(`int`): The height of the canvas in pixels
 /// 3. `shared_memory_name_ptr` (`char []`): The name of the shared memory region to create/use.
+///
+/// # Safety
+///
+/// No special handling needed
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn breakwater_init_original_parser(
     width: c_int,
@@ -48,6 +52,10 @@ pub unsafe extern "C" fn breakwater_init_original_parser(
 /// Function is thread safe (I guess).
 ///
 /// Function has no arguments.
+///
+/// # Safety
+///
+/// Call [`breakwater_original_parser_parser_lookahead`] before.
 #[unsafe(no_mangle)]
 pub extern "C" fn breakwater_original_parser_parser_lookahead() -> size_t {
     let parser = ORIGINAL_PARSER
@@ -70,6 +78,10 @@ pub extern "C" fn breakwater_original_parser_parser_lookahead() -> size_t {
 ///    forming the response to the client. **It's your responsibility to free the passed memory!**
 /// 4. `out_response_len` (`size_t*`): This number will be changed to the number of bytes the output
 ///    for the client has.
+///
+/// # Safety
+///
+/// Call [`breakwater_original_parser_parser_lookahead`] before.
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn breakwater_original_parser_parse(
     buffer: *mut u8,
